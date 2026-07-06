f49de9e4d94a26c5fe4bb9f45ea906611bfba803
Maychel Hutahaean <maychel.hutahaean@youtap.com>
2026-07-06 15:55:23 +0700

    Merge remote changes

--- script.js
+++ script.js
@@ -1,50 +1,6 @@
 fetch("/api/run")
-
-.then(res=>res.json())
-
-.then(data=>{
-
-const total =
-data.passed_count+
-data.failed_count+
-data.blocked_count+
-data.untested_count+
-data.retest_count;
-
-const passRate =
-Math.round(data.passed_count/total*100);
-
-document.getElementById("passrate").innerHTML=
-passRate+"% Passed";
-
-new Chart(document.getElementById("pie"),{
-
-type:"pie",
-
-data:{
-
-labels:[
-"Passed",
-"Failed",
-"Blocked",
-"Untested",
-"Retest"
-],
-
-datasets:[{
-
-data:[
-data.passed_count,
-data.failed_count,
-data.blocked_count,
-data.untested_count,
-data.retest_count
-]
-
-}]
-
-}
-
-});
-
-});
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+    alert(JSON.stringify(data, null, 2));
+  });
